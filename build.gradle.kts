a0bce4984706fb2c23608e989bf447d920f3ef5e
Pavlo Davydiuk <pavlo.davydiuk@allegro.pl>
2022-01-26 10:07:15 +0100

    Make XCFramework by using gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 import io.gitlab.arturbosch.detekt.Detekt
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
     kotlin("multiplatform") version Versions.kotlin
@@ -29,27 +29,39 @@ kotlin {
         testRuns["test"].executionTask.configure {
             useJUnitPlatform()
         }
+    }
 
-        val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-            if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
-                ::iosArm64
-            else
-                ::iosX64
 
-        iosTarget("ios") {
-            binaries {
-                framework {
-                    baseName = LibConfig.name
-                    isStatic = true
-                }
-            }
+    val xcFramework = XCFramework(LibConfig.name)
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = LibConfig.name
+            isStatic = true
+            xcFramework.add(this)
         }
     }
+
     sourceSets {
+        val commonMain by getting
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
     }
 }
