6047b2f2c8f3c8ddebf7897ba39be1e0d3240637
Rafal Kapela <rafal.kapela@allegro.pl>
2022-01-27 08:39:51 +0100

    Minor code simplification

--- src/commonMain/kotlin/expressions/Missing.kt
+++ src/commonMain/kotlin/expressions/Missing.kt
@@ -6,10 +6,8 @@ internal object Missing : LogicExpression {
     override val key: String = "missing"
 
     override fun invoke(expression: Any?, data: Any?): List<Any?> {
-        return arrayListOf<Any?>().apply {
-            (expression as? List<Any?>)?.forEach {
-                if (Var(it, data) == null) add(it)
-            }
-        }
+        return (expression as? List<Any?>)?.mapNotNull {
+            it.takeIf { Var(it, data) == null }
+        }.orEmpty()
     }
 }
--- src/commonMain/kotlin/expressions/MissingSome.kt
+++ src/commonMain/kotlin/expressions/MissingSome.kt
@@ -6,9 +6,10 @@ import intValue
 internal object MissingSome : LogicExpression {
     override val key: String = "missing_some"
 
-    override fun invoke(expression: Any?, data: Any?): Any? {
+    override fun invoke(expression: Any?, data: Any?): Any {
         val min = (expression as? List<Any?>?)?.firstOrNull()?.toString()?.intValue ?: 0
-        val keys = (expression as? List<Any?>?)?.getOrNull(1) as? List<Any?> ?: listOf()
+        val keys = ((expression as? List<Any?>?)?.getOrNull(1) as? List<Any?>).orEmpty()
         val missing = Missing(keys, data)
-        return if (keys.size - missing.size >= min) listOf() else missing }
+        return missing.takeIf { keys.size - missing.size < min }.orEmpty()
+    }
 }
