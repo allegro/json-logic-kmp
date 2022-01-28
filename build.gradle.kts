d2cf82ba3932ab5150a2d87c1b59a8f211939041
Pavlo Davydiuk <pavlo.davydiuk@allegro.pl>
2022-01-28 08:29:17 +0100

    Add release workflow, fix building

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,3 +134,12 @@ System.getenv("GPG_KEY_ID")?.let { gpgKeyId ->
         sign(publishing.publications)
     }
 }
+
+
+/*
+We need to disable this task, because it fails with following error:
+`the path does not point to a valid debug symbols file`.
+Currently we are using only Release XCFramework.
+It could be fixed also by `isStatic = false` but we want to get static lib.
+ */
+tasks.getByName("assembleJsonLogicKMPDebugXCFramework").enabled = false
