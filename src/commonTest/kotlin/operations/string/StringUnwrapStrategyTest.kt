c45fda910ca28957287a6a22844b9920415592c9
Rafal Kapela <rafal.kapela@allegro.pl>
2022-02-15 08:53:24 +0100

    String unwrap strategy unit tests cases

--- src/commonTest/kotlin/operations/string/StringUnwrapStrategyTest.kt
+++ src/commonTest/kotlin/operations/string/StringUnwrapStrategyTest.kt
@@ -7,13 +7,77 @@ import io.kotest.matchers.shouldBe
 class StringUnwrapStrategyTest : BehaviorSpec({
     val strategyImplementation: StringUnwrapStrategy = object : StringUnwrapStrategy {}
 
-    given("A false value") {
+    given("A non-decimal double") {
+        val wrappedValue = listOf(2.0)
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValues(wrappedValue)
+            then("should be int string") {
+                unwrapResult shouldHaveSize 1
+                unwrapResult.first() shouldBe "2"
+            }
+        }
+    }
+
+    given("A double") {
+        val wrappedValue = listOf(2.51)
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValues(wrappedValue)
+            then("should be double string") {
+                unwrapResult shouldHaveSize 1
+                unwrapResult.first() shouldBe "2.51"
+            }
+        }
+    }
+    given("A empty list") {
+        val wrappedValue = emptyList<String>()
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValues(wrappedValue)
+            then("should be empty list") {
+                unwrapResult shouldBe emptyList()
+            }
+        }
+    }
+
+    given("A boolean value") {
         val wrappedValue = listOf(false)
         `when`("unwrapped") {
             val unwrapResult = strategyImplementation.unwrapValues(wrappedValue)
-            then("should be null") {
+            then("should be string") {
                 unwrapResult shouldHaveSize 1
-                unwrapResult.first() shouldBe null
+                unwrapResult.first() shouldBe "false"
+            }
+        }
+    }
+
+    given("A null") {
+        val wrappedValue = listOf(null)
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValues(wrappedValue)
+            then("should be empty string") {
+                unwrapResult shouldHaveSize 1
+                unwrapResult.first() shouldBe ""
+            }
+        }
+    }
+
+    given("A nested list items") {
+        val wrappedValue = listOf(listOf(listOf("banana", "apple", "pineapple")))
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValues(wrappedValue)
+            then("should be separated with commas") {
+                unwrapResult shouldHaveSize 1
+                unwrapResult.first() shouldBe "banana,apple,pineapple"
+            }
+        }
+    }
+
+    given("A nested lists items") {
+        val wrappedValue = listOf(true, listOf(listOf("banana")), listOf(2.0))
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValues(wrappedValue)
+            then("should be flattened") {
+                unwrapResult shouldHaveSize 3
+                unwrapResult shouldBe listOf("true", "banana", "2")
             }
         }
     }
