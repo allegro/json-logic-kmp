88fc3300dea6df8d4fe9c9dbab569b51e3576489
Rafal Kapela <rafal.kapela@allegro.pl>
2022-02-15 10:12:39 +0100

    Function rename - cr change

--- src/commonMain/kotlin/operations/numeric/Min.kt
+++ src/commonMain/kotlin/operations/numeric/Min.kt
@@ -5,5 +5,5 @@ import LogicOperation
 internal object Min : LogicOperation, DoubleTypeSensitiveOperation {
     override val key: String = "min"
 
-    override fun invoke(expression: Any?, data: Any?): Any? = resultOrNull(expression) { it.minOrNull() }
+    override fun invoke(expression: Any?, data: Any?): Any? = doubleResultOrNull(expression) { it.minOrNull() }
 }
