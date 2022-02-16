b998b495daedd8129a9470a065ae65c34ab75239
Rafal Kapela <rafal.kapela@allegro.pl>
2022-02-16 11:23:51 +0100

    More and, or operations unit tests cases

--- src/commonTest/kotlin/operations/logic/AndTest.kt
+++ src/commonTest/kotlin/operations/logic/AndTest.kt
@@ -1,5 +1,6 @@
 package operations.logic
 
+import JsonLogicEngine
 import TestInput
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.datatest.withData
@@ -12,25 +13,78 @@ class AndTest : FunSpec({
             nameFn = { "Should apply ${it.data} on ${it.expression} result in ${it.result}" },
             // given
             ts = listOf(
-                TestInput(mapOf("and" to listOf(true, true)), null, true),
-                TestInput(mapOf("and" to listOf(true, false)), null, false),
-                TestInput(mapOf("and" to listOf(false, true)), null, false),
-                TestInput(mapOf("and" to listOf(false, false)), null, false),
-                TestInput(mapOf("and" to listOf(true, true, true)), null, true),
-                TestInput(mapOf("and" to listOf(true, true, false)), null, false),
-                TestInput(mapOf("and" to listOf(false)), null, false),
-                TestInput(mapOf("and" to listOf(true)), null, true),
-                TestInput(mapOf("and" to listOf(1, 3)), null, 3),
-                TestInput(mapOf("and" to listOf(3, false)), null, false),
-                TestInput(mapOf("and" to listOf(false, 3)), null, false),
-                TestInput(mapOf("and" to listOf(emptyList<Any>(), true)), null, emptyList<Any>()),
-                TestInput(mapOf("and" to listOf(0, true)), null, 0),
-                TestInput(mapOf("and" to listOf("", true)), null, ""),
-                TestInput(mapOf("and" to listOf("0", true)), null, true),
-                TestInput(mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), true)), null, true),
-                TestInput(mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), false)), null, false),
-                TestInput(mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), mapOf("!" to true))), null, false),
-                TestInput(mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), mapOf("<" to listOf(1, 3)))), null, true),
+                TestInput(expression = mapOf("and" to listOf(true, false)), data = null, result = false),
+                TestInput(expression = mapOf("and" to listOf(true, true)), data = null, result = true),
+                TestInput(expression = mapOf("and" to listOf(false, true)), data = null, result = false),
+                TestInput(expression = mapOf("and" to listOf(false, false)), data = null, result = false),
+                TestInput(expression = mapOf("and" to listOf(true, true, true)), data = null, result = true),
+                TestInput(expression = mapOf("and" to listOf(true, true, false)), data = null, result = false),
+                TestInput(expression = mapOf("and" to listOf(false)), data = null, result = false),
+                TestInput(expression = mapOf("and" to listOf(true)), data = null, result = true),
+                TestInput(expression = mapOf("and" to listOf(1, 3)), data = null, result = 3),
+                TestInput(expression = mapOf("and" to listOf(3, false)), data = null, result = false),
+                TestInput(expression = mapOf("and" to listOf(false, 3)), data = null, result = false),
+                TestInput(
+                    expression = mapOf("and" to listOf(emptyList<Any>(), true)),
+                    data = null,
+                    result = emptyList<Any>()
+                ),
+                TestInput(expression = mapOf("and" to listOf(0, true)), data = null, result = 0),
+                TestInput(expression = mapOf("and" to listOf("", true)), data = null, result = ""),
+                TestInput(expression = mapOf("and" to listOf("0", true)), data = null, result = true),
+                TestInput(
+                    expression = mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), true)),
+                    data = null,
+                    result = true
+                ),
+                TestInput(
+                    expression = mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), false)),
+                    data = null,
+                    result = false
+                ),
+                TestInput(
+                    expression = mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), mapOf("!" to true))),
+                    data = null,
+                    result = false
+                ),
+                TestInput(
+                    expression = mapOf("and" to listOf(mapOf(">" to listOf(3, 1)), mapOf("<" to listOf(1, 3)))),
+                    data = null,
+                    result = true
+                ),
+                TestInput(
+                    expression = mapOf("and" to listOf("0", listOf("banana"))),
+                    data = null,
+                    result = listOf("banana")
+                ),
+                TestInput(
+                    expression = mapOf("and" to listOf(listOf("grapes"), listOf("banana"))),
+                    data = null,
+                    result = listOf("banana")
+                ),
+                TestInput(expression = mapOf("and" to listOf(false, listOf("banana"))), data = null, result = false),
+                TestInput(
+                    expression = mapOf("and" to listOf(true, listOf("banana"))),
+                    data = null,
+                    result = listOf("banana")
+                ),
+                TestInput(
+                    expression = mapOf("and" to listOf(listOf(null), listOf("banana"))),
+                    data = null,
+                    result = listOf("banana")
+                ),
+                TestInput(expression = mapOf("and" to listOf(listOf(null), true)), data = null, result = true),
+                TestInput(expression = mapOf("and" to listOf(listOf(null), false)), data = null, result = false),
+                TestInput(
+                    expression = mapOf("and" to listOf(listOf(emptyList<String>()), listOf("banana"))),
+                    data = null,
+                    result = listOf("banana")
+                ),
+                TestInput(
+                    expression = mapOf("and" to listOf(listOf(emptyList<String>()), listOf(null))),
+                    data = null,
+                    result = listOf(null)
+                ),
             )
         ) { (expression, data, result) ->
             // when
--- src/commonTest/kotlin/operations/logic/OrTest.kt
+++ src/commonTest/kotlin/operations/logic/OrTest.kt
@@ -13,21 +13,58 @@ class OrTest : FunSpec({
             nameFn = { "Should apply ${it.data} on ${it.expression} result in ${it.result}" },
             // given
             ts = listOf(
-                TestInput(mapOf("or" to listOf(true, true)), null, true),
-                TestInput(mapOf("or" to listOf(false, true)), null, true),
-                TestInput(mapOf("or" to listOf(true, false)), null, true),
-                TestInput(mapOf("or" to listOf(false, false)), null, false),
-                TestInput(mapOf("or" to listOf(false, false, true)), null, true),
-                TestInput(mapOf("or" to listOf(false, false, false)), null, false),
-                TestInput(mapOf("or" to listOf(false)), null, false),
-                TestInput(mapOf("or" to listOf(true)), null, true),
-                TestInput(mapOf("or" to listOf(1, 3)), null, 1),
-                TestInput(mapOf("or" to listOf(3, false)), null, 3),
-                TestInput(mapOf("or" to listOf(false, 3)), null, 3),
-                TestInput(mapOf("or" to listOf(emptyList<String>(), true)), null, true),
-                TestInput(mapOf("or" to listOf(0, true)), null, true),
-                TestInput(mapOf("or" to listOf("", true)), null, true),
-                TestInput(mapOf("or" to listOf("0", true)), null, "0"),
+                TestInput(expression = mapOf("or" to listOf(true, true)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf(false, true)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf(true, false)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf(false, false)), data = null, result = false),
+                TestInput(expression = mapOf("or" to listOf(false, false, true)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf(false, false, false)), data = null, result = false),
+                TestInput(expression = mapOf("or" to listOf(false)), data = null, result = false),
+                TestInput(expression = mapOf("or" to listOf(true)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf(1, 3)), data = null, result = 1),
+                TestInput(expression = mapOf("or" to listOf(3, false)), data = null, result = 3),
+                TestInput(expression = mapOf("or" to listOf(false, 3)), data = null, result = 3),
+                TestInput(expression = mapOf("or" to listOf(emptyList<String>(), true)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf(0, true)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf("", true)), data = null, result = true),
+                TestInput(expression = mapOf("or" to listOf("0", true)), data = null, result = "0"),
+                TestInput(
+                    expression = mapOf("or" to listOf("0", listOf("banana"))),
+                    data = null,
+                    result = "0"
+                ),
+                TestInput(
+                    expression = mapOf("or" to listOf(listOf("grapes"), listOf("banana"))),
+                    data = null,
+                    result = listOf("grapes")
+                ),
+                TestInput(
+                    expression = mapOf("or" to listOf(false, listOf("banana"))),
+                    data = null,
+                    result = listOf("banana")
+                ),
+                TestInput(
+                    expression = mapOf("or" to listOf(true, listOf("banana"))),
+                    data = null,
+                    result = true
+                ),
+                TestInput(
+                    expression = mapOf("or" to listOf(listOf(null), listOf("banana"))),
+                    data = null,
+                    result = listOf(null)
+                ),
+                TestInput(expression = mapOf("or" to listOf(listOf(null), true)), data = null, result = listOf(null)),
+                TestInput(expression = mapOf("or" to listOf(listOf(null), false)), data = null, result = listOf(null)),
+                TestInput(
+                    expression = mapOf("or" to listOf(listOf(emptyList<String>()), listOf("banana"))),
+                    data = null,
+                    result = listOf(emptyList<String>())
+                ),
+                TestInput(
+                    expression = mapOf("or" to listOf(listOf(emptyList<String>()), listOf(null))),
+                    data = null,
+                    result = listOf(emptyList<String>())
+                ),
             )
         ) { (expression, data, result) ->
             // when
