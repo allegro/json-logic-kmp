62ebb890e5f6d0721e5330e7b70b757ae79c2404
Rafal Kapela <rafal.kapela@allegro.pl>
2022-02-21 14:52:43 +0100

    Equals unwrap strategy unit tests

--- src/commonTest/kotlin/operations/logic/unwrap/EqualsUnwrapStrategyTest.kt
+++ src/commonTest/kotlin/operations/logic/unwrap/EqualsUnwrapStrategyTest.kt
@@ -1,89 +1,118 @@
-//package operations.logic.unwrap
-//
-//import io.kotest.core.spec.style.BehaviorSpec
-//import io.kotest.matchers.shouldBe
-//import operations.logic.unwrap.TruthyUnwrapStrategy
-//
-//class EqualsUnwrapStrategyTest : BehaviorSpec({
-//    val strategyImplementation: TruthyUnwrapStrategy = object : TruthyUnwrapStrategy {}
-//
-//    given("A string zero") {
-//        val wrappedValue = "0"
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be true") {
-//                unwrapResult shouldBe true
-//            }
-//        }
-//    }
-//
-//    given("A null") {
-//        val wrappedValue = null
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be false") {
-//                unwrapResult shouldBe false
-//            }
-//        }
-//    }
-//
-//    given("Any non-empty string") {
-//        val wrappedValue = "anything"
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be true") {
-//                unwrapResult shouldBe true
-//            }
-//        }
-//    }
-//
-//    given("An empty string") {
-//        val wrappedValue = ""
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be false") {
-//                unwrapResult shouldBe false
-//            }
-//        }
-//    }
-//
-//    given("A non-empty list") {
-//        val wrappedValue = listOf("banana", "strawberry")
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be true") {
-//                unwrapResult shouldBe true
-//            }
-//        }
-//    }
-//
-//    given("An empty list") {
-//        val wrappedValue = emptyList<Any>()
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be false") {
-//                unwrapResult shouldBe false
-//            }
-//        }
-//    }
-//
-//    given("Any non-zero number") {
-//        val wrappedValue = -1
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be true") {
-//                unwrapResult shouldBe true
-//            }
-//        }
-//    }
-//
-//    given("A zero number") {
-//        val wrappedValue = 0
-//        `when`("unwrapped") {
-//            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
-//            then("should be false") {
-//                unwrapResult shouldBe false
-//            }
-//        }
-//    }
-//})
+package operations.logic.unwrap
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class EqualsUnwrapStrategyTest : BehaviorSpec({
+    val strategyImplementation: EqualsUnwrapStrategy = object : EqualsUnwrapStrategy {}
+
+    given("A number") {
+        val wrappedValue = 20
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should has the same decimal value") {
+                unwrapResult shouldBe 20.0
+            }
+        }
+    }
+
+    given("A number string") {
+        val wrappedValue = "12"
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should has the decimal value") {
+                unwrapResult shouldBe 12.0
+            }
+        }
+    }
+
+    given("Any string") {
+        val wrappedValue = "banana"
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should has the same value") {
+                unwrapResult shouldBe "banana"
+            }
+        }
+    }
+
+    given("A list with single null") {
+        val wrappedValue = listOf(null)
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be 0") {
+                unwrapResult shouldBe 0.0
+            }
+        }
+    }
+
+    given("A list with single boolean") {
+        val wrappedValue = listOf(true)
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be the same value") {
+                unwrapResult shouldBe listOf(true)
+            }
+        }
+    }
+
+    given("A list with single value") {
+        val wrappedValue = listOf(20)
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be flattened value") {
+                unwrapResult shouldBe 20.0
+            }
+        }
+    }
+
+    given("A list with multiple values") {
+        val wrappedValue = listOf(2, "banana")
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be the same") {
+                unwrapResult shouldBe listOf(2, "banana")
+            }
+        }
+    }
+
+    given("An empty list") {
+        val wrappedValue = emptyList<String>()
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be the same") {
+                unwrapResult shouldBe emptyList<String>()
+            }
+        }
+    }
+
+    given("A true value") {
+        val wrappedValue = true
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be 1") {
+                unwrapResult shouldBe 1.0
+            }
+        }
+    }
+
+    given("A false value") {
+        val wrappedValue = false
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be 0") {
+                unwrapResult shouldBe 0.0
+            }
+        }
+    }
+
+    given("Any other type value") {
+        val wrappedValue = Pair(1, 2)
+        `when`("unwrapped") {
+            val unwrapResult = strategyImplementation.unwrapValue(wrappedValue)
+            then("should be the same") {
+                unwrapResult shouldBe Pair(1, 2)
+            }
+        }
+    }
+})
