c099e31b2f8e7723f209c539710b21db6b43033d
Rafal Kapela <rafal.kapela@allegro.pl>
2022-02-24 11:04:15 +0100

    Detekt fix

--- src/commonMain/kotlin/operations/logic/NotStrictEquals.kt
+++ src/commonMain/kotlin/operations/logic/NotStrictEquals.kt
@@ -5,6 +5,7 @@ import operations.LogicOperation
 internal object NotStrictEquals : LogicOperation, StrictEqualsOperation {
     override val key: String = "!=="
 
-    override fun invoke(expression: Any?, data: Any?): Boolean = !compare(expression) { first, second -> first == second }
+    override fun invoke(expression: Any?, data: Any?): Boolean =
+        !compare(expression) { first, second -> first == second }
 }
 
