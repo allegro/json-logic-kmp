a8913b4bc78b412e7747578ea6991177fb4b9d52
Rafal Kapela <rafal.kapela@allegro.pl>
2022-03-08 13:29:45 +0100

    Junit report optimisation

---
+++ src/commonTest/kotlin/ProjectTestsConfig.kt
@@ -0,0 +1,7 @@
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.extensions.Extension
+import io.kotest.core.names.DuplicateTestNameMode
+
+object ProjectTestsConfig : AbstractProjectConfig() {
+    override val duplicateTestNameMode = DuplicateTestNameMode.Silent
+}
