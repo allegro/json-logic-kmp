f6b29c630cddc47399f219f42c4d2ada457f127b
Rafal Kapela <rafal.kapela@allegro.pl>
2022-03-10 15:00:35 +0100

    added single new line

--- src/commonTest/kotlin/LogicOperationTest.kt
+++ src/commonTest/kotlin/LogicOperationTest.kt
@@ -23,6 +23,7 @@ abstract class LogicOperationTest(
             evaluationResult.value shouldBe result
         }
     }
+
     context("$testName - failure results") {
         withData(
             nameFn = nameFunction,
