cdb93df8eb2a9f4cec505877cd8c26ed9899536b
Rafal Kapela <rafal.kapela@allegro.pl>
2022-03-15 13:20:39 +0100

    Suppression for unchecked casts

--- src/commonMain/kotlin/operations/array/unwrap/EvaluatingUnwrapStrategy.kt
+++ src/commonMain/kotlin/operations/array/unwrap/EvaluatingUnwrapStrategy.kt
@@ -6,6 +6,7 @@ internal interface EvaluatingUnwrapStrategy : LogicEvaluator {
     fun unwrapOperationData(expression: List<Any?>, data: Any?) =
         (expression.firstOrNull().unwrapOperationData(data) as? List<*>)
 
+    @Suppress("UNCHECKED_CAST")
     private fun Any?.unwrapOperationData(data: Any?): Any? = when {
         this is List<*> -> map { it.unwrapOperationData(data) }
         isExpression(this) -> evaluateLogic(this as Map<String, Any?>, data)
