7d67f0ec26f317eabda361d319f3ee01a2f6d5b3
Rafal Kapela <rafal.kapela@allegro.pl>
2022-04-28 13:40:33 +0200

    Equals tests revert

--- core/src/commonTest/kotlin/operations/logic/EqualsTest.kt
+++ core/src/commonTest/kotlin/operations/logic/EqualsTest.kt
@@ -67,6 +67,165 @@ class EqualsTest : FunSpec({
                     expression = mapOf("==" to listOf(listOf("banana"), null)),
                     result = JsonLogicResult.Success(false)
                 ),
+                TestInput(
+                    expression = mapOf("==" to listOf(listOf("banana"), true)),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(listOf("banana"), false)),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(true, false)),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(false, true)),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(expression = mapOf("==" to listOf(true, true)), result = JsonLogicResult.Success(true)),
+                TestInput(
+                    expression = mapOf("==" to listOf(1, listOf("1"))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(1, listOf(listOf("1")))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("banana", listOf(listOf("banana")))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("banana", listOf("banana", "banana"))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(1, listOf(1))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(expression = mapOf("==" to listOf(1, false)), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf(-1, false)), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf(0, false)), result = JsonLogicResult.Success(true)),
+                TestInput(expression = mapOf("==" to listOf(0, true)), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf(1, true)), result = JsonLogicResult.Success(true)),
+                TestInput(expression = mapOf("==" to listOf(-1, true)), result = JsonLogicResult.Success(false)),
+                TestInput(
+                    expression = mapOf("==" to listOf(true, true, false)),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(expression = mapOf("==" to listOf(1, 0, 1)), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf(1)), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf(true)), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to true), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to false), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf("true")), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf("banana")), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to "banana"), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to listOf(null)), result = JsonLogicResult.Success(true)),
+                TestInput(expression = mapOf("==" to null), result = JsonLogicResult.Success(true)),
+                TestInput(expression = mapOf("==" to ""), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to "     "), result = JsonLogicResult.Success(false)),
+                TestInput(expression = mapOf("==" to emptyList<Any>()), result = JsonLogicResult.Success(true)),
+                TestInput(
+                    expression = mapOf("==" to listOf(emptyList<Any>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(expression = mapOf("==" to listOf(null, null)), result = JsonLogicResult.Success(true)),
+                TestInput(
+                    expression = mapOf("==" to listOf(listOf(null), listOf(null))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(null, listOf(null))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(1, listOf(null))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(0, listOf(null))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(true, listOf(null))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(false, listOf(null))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(listOf(false), listOf(false))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(-1, listOf(null))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(0.5, listOf(null))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(expression = mapOf("==" to listOf("", "")), result = JsonLogicResult.Success(true)),
+                TestInput(expression = mapOf("==" to listOf("", "    ")), result = JsonLogicResult.Success(false)),
+                TestInput(
+                    expression = mapOf("==" to listOf("", listOf(""))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(listOf(""), listOf(""))),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("", listOf(listOf("")))),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("", emptyList<String>())),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(false, emptyList<String>())),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(0, emptyList<String>())),
+                    result = JsonLogicResult.Success(true)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("0", emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("0.0", emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(1, emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("1", emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("1.0", emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf(-1, emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("-1", emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
+                TestInput(
+                    expression = mapOf("==" to listOf("-1.0", emptyList<String>())),
+                    result = JsonLogicResult.Success(false)
+                ),
             )
         )
     }
