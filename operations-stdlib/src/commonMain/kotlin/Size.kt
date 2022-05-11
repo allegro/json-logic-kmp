5cdc9376c1adeebaf7b9e667700e6b378b6b1549
mkrogulski <marek.krogulski@allegrogroup.com>
2022-05-11 13:12:39 +0200

    added more tests for size, updates some implementation to handle edge case

--- operations-stdlib/src/commonMain/kotlin/Size.kt
+++ operations-stdlib/src/commonMain/kotlin/Size.kt
@@ -1,8 +1,12 @@
+import utils.asList
+import utils.isSingleNullList
+
 object Size : StandardLogicOperation {
     override fun invoke(expression: Any?, data: Any?): Any? {
-        return when (expression) {
-            is Map<*, *> -> expression.size
-            is List<*> -> expression.size
+        return when (val firstItem = expression.asList.firstOrNull()) {
+            firstItem.isSingleNullList() -> null
+            is Map<*, *> -> firstItem.size
+            is List<*> -> firstItem.size
             else -> null
         }
     }
--- operations-stdlib/src/commonTest/kotlin/SizeTest.kt
+++ operations-stdlib/src/commonTest/kotlin/SizeTest.kt
@@ -1,5 +1,6 @@
 import JsonLogicResult.Success
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.test.TestResult
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
@@ -10,11 +11,11 @@ class SizeTest : FunSpec({
         nameFn = { input -> "Should evaluated ${input.expression} with given ${input.data} result in ${input.result}" },
         ts = listOf(
             TestInput(
-                expression = mapOf("size" to "test"),
+                expression = mapOf("size" to listOf(listOf("test"))),
                 result = Success(1)
             ),
             TestInput(
-                expression = mapOf("size" to listOf("test", "test2")),
+                expression = mapOf("size" to listOf(listOf("test", "test2"), "a")),
                 result = Success(2)
             ),
             TestInput(
@@ -25,27 +26,67 @@ class SizeTest : FunSpec({
                     )
                 ),
                 data = mapOf("fruits" to listOf(listOf("apple"), "banana", "pineapple"), "fruits2" to "testFruit"),
-                result = Success(2)
+                result = Success(3)
             ),
             TestInput(
                 expression = mapOf(
                     "size" to mapOf("var" to "fruits")
                 ),
-                data = mapOf("fruits" to listOf(listOf("apple"), "banana", "pineapple")),
+                data = mapOf("fruits" to listOf(listOf(listOf("apple"), "banana", "pineapple"))),
                 result = Success(3)
+            ),
+            TestInput(
+                expression = mapOf(
+                    "size" to mapOf(
+                        "if" to listOf(
+                            mapOf("<" to listOf(mapOf("var" to "temp"), 0)),
+                            "freezing",
+                            mapOf("<" to listOf(mapOf("var" to "temp"), 100)),
+                            listOf(listOf("liquid", "test", "b", "c")),
+                            "gas"
+                        )
+                    )
+                ),
+                data = mapOf("temp" to 55),
+                result = Success(4)
+            ),
+            TestInput(
+                expression = mapOf(
+                    "size" to mapOf(
+                        "if" to listOf(
+                            mapOf("<" to listOf(mapOf("var" to "temp"), 100)),
+                            listOf("liquid", "test", "b", "c"),
+                            "gas"
+                        )
+                    )
+                ),
+                data = mapOf("temp" to 55),
+                result = JsonLogicResult.NullResultFailure
             ),
             TestInput(
                 expression = mapOf(
                     "size" to 1
                 ),
-                result = Success(1)
+                result = JsonLogicResult.NullResultFailure
             ),
-                TestInput(
+            TestInput(
                 expression = mapOf(
                     "size" to 1521414312
                 ),
-            result = Success(1)
-        )
+                result = JsonLogicResult.NullResultFailure
+            ),
+            TestInput(
+                expression = mapOf(
+                    "size" to null
+                ),
+                result = JsonLogicResult.NullResultFailure
+            ),
+            TestInput(
+                expression = mapOf(
+                    "size" to "aa"
+                ),
+                result = JsonLogicResult.NullResultFailure
+            ),
         )
         // given
     ) { testInput: TestInput ->
