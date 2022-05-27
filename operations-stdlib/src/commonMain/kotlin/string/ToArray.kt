28fa6a9a3d46dda7b4b1b666d35526a363bfb44b
Rafal Kapela <rafal.kapela@allegro.pl>
2022-05-27 11:39:09 +0200

    ToArray operation template

---
+++ operations-stdlib/src/commonMain/kotlin/string/ToArray.kt
@@ -0,0 +1,9 @@
+package string
+
+import operation.StandardLogicOperation
+
+object ToArray : StandardLogicOperation {
+    override fun evaluateLogic(expression: Any?, data: Any?): Any? {
+//TODO("not implemented")
+    }
+}
