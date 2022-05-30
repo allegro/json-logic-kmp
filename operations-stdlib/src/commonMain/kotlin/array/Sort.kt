b4cfd3f4271ef78caebaf388248ac095b5101499
Rafal Kapela <rafal.kapela@allegro.pl>
2022-05-30 14:24:21 +0200

    Proper enum usage

--- operations-stdlib/src/commonMain/kotlin/array/Sort.kt
+++ operations-stdlib/src/commonMain/kotlin/array/Sort.kt
@@ -9,25 +9,31 @@ object Sort : StandardLogicOperation {
     override fun evaluateLogic(expression: Any?, data: Any?): Any? =
         with(expression.asList) {
             val elementsToSort = firstOrNull()
-            val sortingMode = secondOrNull()
+            val sortingMode = (secondOrNull() as? String).toSortOrder()
 
             sortIfNumbers(elementsToSort?.asList, sortingMode)
         }
 
-    private fun sortIfNumbers(listToSort: List<Any?>?, mode: Any?) = listToSort?.filterIsInstance<Number>()
+    private fun String?.toSortOrder() = when (this) {
+        SortOrder.DESC.order -> SortOrder.DESC
+        SortOrder.ASC.order -> SortOrder.ASC
+        else -> null
+    }
+
+    private fun sortIfNumbers(listToSort: List<Any?>?, mode: SortOrder?) = listToSort?.filterIsInstance<Number>()
         .takeIf { it?.size == listToSort?.size }
         ?.sortWithMode(mode)
 
-    private fun List<Number>.sortWithMode(sortingMode: Any?) = asDoubleList.filterNotNull().let {
+    private fun List<Number>.sortWithMode(sortingMode: SortOrder?) = asDoubleList.filterNotNull().let {
         when (sortingMode) {
-            SortOrder.DESC.order -> it.sortedDescending()
-            SortOrder.ASC.order -> it.sorted()
+            SortOrder.DESC -> it.sortedDescending()
+            SortOrder.ASC -> it.sorted()
             else -> null
         }
     }
-}
 
-private enum class SortOrder(val order: String) {
-    DESC("desc"),
-    ASC("asc")
+    private enum class SortOrder(val order: String) {
+        DESC("desc"),
+        ASC("asc")
+    }
 }
