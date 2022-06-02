22c5d8f58446bc55e37dc88a137cadf47cf55655
Rafal Kapela <rafal.kapela@allegro.pl>
2022-06-02 12:08:55 +0200

    index consts

--- operations-stdlib/src/commonMain/kotlin/array/JoinToString.kt
+++ operations-stdlib/src/commonMain/kotlin/array/JoinToString.kt
@@ -4,6 +4,13 @@ import operation.StandardLogicOperation
 import utils.asList
 
 object JoinToString : StandardLogicOperation {
+    private const val ELEMENTS_ARG_INDEX = 0
+    private const val SEPARATOR_ARG_INDEX = 1
+    private const val PREFIX_ARG_INDEX = 2
+    private const val POSTFIX_ARG_INDEX = 3
+    private const val LIMIT_ARG_INDEX = 4
+    private const val TRUNCATED_ARG_INDEX = 5
+
     override fun evaluateLogic(expression: Any?, data: Any?): Any? = expression.asList.toOperationArguments()?.invoke()
 
     private fun JoinToStringArguments.invoke() =
@@ -12,12 +19,12 @@ object JoinToString : StandardLogicOperation {
     private fun List<Any?>.toOperationArguments(): JoinToStringArguments? = kotlin.runCatching {
         checkLimitArg()?.let { limit ->
             JoinToStringArguments(
-                elementsToJoin = get(0).asList,
-                separator = get(1) as String,
-                prefix = get(2) as String,
-                postfix = get(3) as String,
+                elementsToJoin = get(ELEMENTS_ARG_INDEX).asList,
+                separator = get(SEPARATOR_ARG_INDEX) as String,
+                prefix = get(PREFIX_ARG_INDEX) as String,
+                postfix = get(POSTFIX_ARG_INDEX) as String,
                 limit = limit,
-                truncated = get(5) as String
+                truncated = get(TRUNCATED_ARG_INDEX) as String
             )
         }
     }.fold(
@@ -25,7 +32,7 @@ object JoinToString : StandardLogicOperation {
         onFailure = { null }
     )
 
-    private fun List<Any?>.checkLimitArg() = (get(4) as Number).takeIf {
+    private fun List<Any?>.checkLimitArg() = (get(LIMIT_ARG_INDEX) as Number).takeIf {
         it.toDouble() == it.toInt().toDouble()
     }?.toInt()
 }
