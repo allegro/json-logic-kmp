ac38966f88e58a900c1f1dc9e990f241f9ee5e26
Rafal Kapela <rafal.kapela@allegro.pl>
2022-07-15 10:09:06 +0200

    Reverse unit tests fix

--- operations-stdlib/src/commonMain/kotlin/Reverse.kt
+++ operations-stdlib/src/commonMain/kotlin/Reverse.kt
@@ -1,12 +1,12 @@
 import operation.StandardLogicOperation
-import utils.asList
 
 object Reverse : StandardLogicOperation {
     override fun evaluateLogic(expression: Any?, data: Any?): Any? {
-        return when (val firstItem = expression.asList.firstOrNull()) {
-            is String -> firstItem.reversed()
-            is List<*> -> firstItem.reversed()
+        return when (expression) {
+            is String -> expression.reversed()
+            is List<*> -> expression.reversed()
             else -> null
+
         }
     }
 }
