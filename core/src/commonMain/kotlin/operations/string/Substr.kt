5963bbd66f6150fd54d5ef7a6e5c03a4fc445649
Rafal Kapela <rafal.kapela@allegro.pl>
2022-08-01 14:51:32 +0200

    code clean up

--- core/src/commonMain/kotlin/operations/string/Substr.kt
+++ core/src/commonMain/kotlin/operations/string/Substr.kt
@@ -20,45 +20,45 @@ internal object Substr : StandardLogicOperation, StringUnwrapStrategy {
         val baseString = unwrapValueAsString(firstOrNull()).joinToString(",")
         return runCatching {
             when {
-                size == 2 -> baseString.startIndexSubstr(startIndex)
+                size == 2 -> baseString.startIndexSubstring(startIndex)
                 size > 2 -> baseString.fromStartIndexToEndIndex(startIndex, charsCount)
                 else -> baseString
             }
         }.getOrNull().orEmpty()
     }
 
-    // new impl
-    // TODO make it prettier
-    private fun String.startIndexSubstr(startIndex: Int): String {
-        return if (startIndex >= 0) {
-            substring(startIndex)
-        } else {
-            if (abs(startIndex) > length) {
-                this
-            } else {
-                substring(length + startIndex)
-            }
-        }
+    private fun String.startIndexSubstring(startIndex: Int): String = when {
+        startIndex >= 0 -> substring(startIndex)
+        abs(startIndex) <= length -> substring(length + startIndex)
+        else -> this
     }
 
     private fun String.fromStartIndexToEndIndex(startIndex: Int, charsCount: Int) = when {
-        startIndex >= 0 && charsCount > 0 -> {
-            val newCount = (startIndex + charsCount).takeIf { it <= length } ?: length
-            substring(startIndex, newCount)
-        }
+        startIndex >= 0 && charsCount > 0 -> notNegativeArgsSubstring(startIndex, charsCount)
         startIndex >= 0 && charsCount < 0 -> substring(startIndex, length + charsCount)
-        startIndex < 0 && charsCount < 0 -> {
-            val newStartIndex = (length + startIndex).takeIf { it >= 0 } ?: 0
-            val newCount = (length + charsCount).takeIf { it <= length } ?: length
-            substring(newStartIndex, newCount)
-        }
-        startIndex < 0 && charsCount > 0 -> {
-            val newStartIndex = (length + startIndex).takeIf { it >= 0 } ?: 0
-            val newCount = if(newStartIndex + charsCount > length) {
-                length
-            } else newStartIndex + charsCount
-            substring(newStartIndex, newCount)
-        }
+        startIndex < 0 && charsCount < 0 -> negativeArgsSubstring(startIndex, charsCount)
+        startIndex < 0 && charsCount > 0 -> negativeStartIndexSubString(startIndex, charsCount)
         else -> null
     }
+
+    private fun String.notNegativeArgsSubstring(startIndex: Int, charsCount: Int): String {
+        val outOfBoundsSafeCount = (startIndex + charsCount).constrainOutOfBoundsCharsCount(length)
+        return substring(startIndex, outOfBoundsSafeCount)
+    }
+
+    private fun String.negativeArgsSubstring(startIndex: Int, charsCount: Int): String {
+        val outOfBoundsSafeStartIndex = constrainNegativeStartIndex(startIndex)
+        val outOfBoundsSafeCount = (length + charsCount).constrainOutOfBoundsCharsCount(length)
+        return substring(outOfBoundsSafeStartIndex, outOfBoundsSafeCount)
+    }
+
+    private fun String.negativeStartIndexSubString(startIndex: Int, charsCount: Int): String {
+        val outOfBoundsSafeStartIndex = constrainNegativeStartIndex(startIndex)
+        val outOfBoundsSafeCount = (outOfBoundsSafeStartIndex + charsCount).constrainOutOfBoundsCharsCount(length)
+        return substring(outOfBoundsSafeStartIndex, outOfBoundsSafeCount)
+    }
+
+    private fun String.constrainNegativeStartIndex(startIndex: Int) = (length + startIndex).takeIf { it >= 0 } ?: 0
+
+    private fun Int.constrainOutOfBoundsCharsCount(sourceStringLength: Int) = takeIf { it <= sourceStringLength } ?: sourceStringLength
 }
