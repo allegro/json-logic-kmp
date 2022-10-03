2a634352a10c036904e500c1c2d4ded455f6cb09
Robert Ogiba <robert.ogiba@allegro.pl>
2022-10-03 11:27:31 +0200

    Add tests for ios version of Encode operation

--- operations-stdlib/src/iosMain/kotlin/encoding/Encode.kt
+++ operations-stdlib/src/iosMain/kotlin/encoding/Encode.kt
@@ -1,14 +1,17 @@
 package encoding
 
 import operation.StandardLogicOperation
+import platform.Foundation.NSCharacterSet
 import string.StringUnwrapStrategy
 import platform.Foundation.NSString
-import platform.Foundation.NSStringEncoding
-import platform.Foundation.stringByAddingPercentEscapesUsingEncoding
+import platform.Foundation.stringByAddingPercentEncodingWithAllowedCharacters
 import platform.Foundation.create
 
 actual object Encode : StandardLogicOperation, StringUnwrapStrategy {
     actual override fun evaluateLogic(expression: Any?, data: Any?): Any? {
-        return unwrapValueAsString(expression)?.let { NSString.create(string = it).stringByAddingPercentEscapesUsingEncoding(NSStringEncoding.MAX_VALUE) }
+        return unwrapValueAsString(expression)?.let {
+            NSString.create(string = it)
+                .stringByAddingPercentEncodingWithAllowedCharacters(allowedCharacters = NSCharacterSet.alphanumericCharacterSet)
+        }
     }
 }
