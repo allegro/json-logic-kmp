72dbbe989ba54c5bf461127fe8c3697089a66ba8
Kamil Strąk <kamil.strak@allegro.pl>
2023-03-30 16:08:15 +0200

    [MBOX-5021] - Compare to date opeartor: - Fix detect

--- operations-stdlib/src/commonMain/kotlin/string/compareToDate/LeapYear.kt
+++ operations-stdlib/src/commonMain/kotlin/string/compareToDate/LeapYear.kt
@@ -1,14 +1,14 @@
 package string.compareToDate
 
+private const val LAP_YEAR_PERIOD = 4
+private const val CENTURY_YEAR_CHECKER = 100
+private const val CENTURY_LEAP_YEAR_CHECKER = 400
 val Int.isLeapYear: () -> Boolean
     get() = {
-        val lapYearPeriod = 4
-        val centuryYearChecker = 100
-        val centuryLeapYearChecker = 400
         when {
-             this.mod(lapYearPeriod) == 0 -> {
+             this.mod(LAP_YEAR_PERIOD) == 0 -> {
                 when {
-                    this.mod(centuryYearChecker) == 0 -> this.mod(centuryLeapYearChecker) == 0
+                    this.mod(CENTURY_YEAR_CHECKER) == 0 -> this.mod(CENTURY_LEAP_YEAR_CHECKER) == 0
                     else -> true
                 }
             }
