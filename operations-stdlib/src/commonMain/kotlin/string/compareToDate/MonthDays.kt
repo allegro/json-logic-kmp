72aabca9b50cd29e7f5e3d9f51ec269524283049
Kamil Strąk <kamil.strak@allegro.pl>
2023-03-31 10:23:23 +0200

    [MBOX-5021] - Improve readability

--- operations-stdlib/src/commonMain/kotlin/string/compareToDate/MonthDays.kt
+++ operations-stdlib/src/commonMain/kotlin/string/compareToDate/MonthDays.kt
@@ -4,6 +4,7 @@ private const val TWENTY_EIGHT_DAYS = 28
 private const val TWENTY_NINE_DAYS = 29
 private const val THIRTY_DAYS = 30
 private const val THIRTY_ONE_DAYS = 31
+
 enum class MonthDays(val standardMax: Int) {
     JANUARY(THIRTY_ONE_DAYS),
     FEBRUARY(TWENTY_EIGHT_DAYS),
