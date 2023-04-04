78d9c34aca135cb14b2e83b02e9ee20991fef519
Kamil Strąk <kamil.strak@allegro.pl>
2023-04-04 13:49:00 +0200

    [MBOX-5021] - Remove unused files

--- operations-stdlib/src/commonMain/kotlin/string/compareToDate/MonthDays.kt
+++
@@ -1,7 +0,0 @@
-package string.compareToDate
-
-private const val TWENTY_EIGHT_DAYS = 28
-private const val TWENTY_NINE_DAYS = 29
-private const val THIRTY_DAYS = 30
-private const val THIRTY_ONE_DAYS = 31
-
